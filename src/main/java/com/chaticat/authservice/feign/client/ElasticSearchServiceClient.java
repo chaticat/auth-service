fa8fb9c2768513618dad7bf3a234362664e72bbe
stevi <trozzato@gmail.com>
2023-05-15 10:28:28 +0300

    added async

--- src/main/java/com/chaticat/authservice/feign/client/ElasticSearchServiceClient.java
+++ src/main/java/com/chaticat/authservice/feign/client/ElasticSearchServiceClient.java
@@ -2,6 +2,7 @@ package com.chaticat.authservice.feign.client;
 
 import com.chaticat.authservice.feign.payload.UserRequest;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @FeignClient(value = "elastic-search-service", url = "${elastic.search.service.url}")
 public interface ElasticSearchServiceClient {
 
+    @Async
     @RequestMapping(method = RequestMethod.POST, value = "/users")
     void saveUser(@RequestBody UserRequest request);
 }
